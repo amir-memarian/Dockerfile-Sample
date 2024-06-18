5e93252eff5c7eac27edf7de7787f5b0ee727227
amir-memarian <memarian.email@gmail.com>
2024-06-18 17:54:35 +0330

    port

--- NodeJS/src/index.js
+++ NodeJS/src/index.js
@@ -4,5 +4,5 @@ app.get('/', (req, res) => res.json({message: 'Hello World!'}))
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('app listening on http://localhost:${PORT}'))
+app.listen(port, () => console.log(`app listening on http://localhost:${PORT}`))
 
